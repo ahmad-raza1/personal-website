785be0f54541332ee0438e73735097bdd188d881
ahmad-raza1 <ahmadraza_@outlook.com>
2023-02-01 14:46:11 +0500

    updated .gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbzz7oiWBFBwynQCr8lyX_xzrlAg1aGldD6plTfyr_d5TfM5oCARm0H32QdBfHAPiu3UVg/exec',
-  apiKey: 'YOUR_SECRET_KEY'
-};
