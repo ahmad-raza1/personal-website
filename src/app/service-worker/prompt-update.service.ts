0cd1c3df7e6763012d293f284dad9a98ad52cbde
ahmad-raza1 <ahmadraza_@outlook.com>
2023-02-12 21:57:00 +0500

    sw

--- src/app/service-worker/prompt-update.service.ts
+++ src/app/service-worker/prompt-update.service.ts
@@ -15,6 +15,7 @@ export class PromptUpdateService {
 
   checkForUpdate(): void {
     if (this.swUpdate.isEnabled) {
+
       // whenever a new version is ready for activation
       this.subs.sink = this.swUpdate.versionUpdates.pipe(
         filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
